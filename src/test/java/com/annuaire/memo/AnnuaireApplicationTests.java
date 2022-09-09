8bb9d661a55ff48d984d377eece914055d84d4c2
hackibosy <hackibosy@gmail.com>
2022-09-09 10:54:54 +0300

    First commit

---
+++ src/test/java/com/annuaire/memo/AnnuaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annuaire.memo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnuaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
